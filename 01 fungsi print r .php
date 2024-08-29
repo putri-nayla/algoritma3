af2ea483cd9b7f5c3b8a81a87dc47021676391a1
putri-nayla <putri20820@gmail.com>
2024-08-29 10:28:29 +0700

    membuat fungsi print

---
+++ 01 fungsi print r .php
@@ -0,0 +1,9 @@
+<?php
+
+$nama = "<br>putri nayla<br>";
+print_r($nama);
+print ("<p>");
+$buah =array ("apel<br>", "pisang<br>", "ceri</br>");
+print_r($buah);
+echo "<p><strong> by putri nayla</strong>";
+?>
