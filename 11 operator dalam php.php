46243a3bd95c9473541a740cf7223c8dfb6d8767
putri-nayla <putri20820@gmail.com>
2024-08-29 20:32:56 +0700

    membuat operator php

---
+++ 11 operator dalam php.php
@@ -0,0 +1,55 @@
+<?php
+
+$a = 7;
+$b = 1;
+
+// penjumlahan
+$c = $a + $b;
+echo "$a + $b = $c";
+echo "<hr>";
+
+$a =9;
+$b =10;
+
+// pengurangan
+$c = $a - $b;
+echo "$a - $b = $c";
+echo "<hr>";
+
+$a =13;
+$b =11;
+
+// Perkalian
+$c = $a * $b;
+echo "$a * $b = $c";
+echo "<hr>";
+
+$a=35;
+$b=2;
+
+
+// Pembagian
+$c = $a / $b;
+echo "$a / $b = $c";
+echo "<hr>";
+
+$a=5;
+$b=6;
+
+// modulus Sisa bagi
+$c = $a % $b;
+echo "$a % $b = $c";
+echo "<hr>";
+
+$a=8;
+$b=4;
+
+// Pangkat
+$c = $a ** $b;
+echo "$a ** $b = $c";
+echo "<hr>";
+$a=2;
+$b=5;
+
+
+?>
