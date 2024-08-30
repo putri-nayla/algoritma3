59c2921b14c8c7b53f88c8aceea805f1289e9575
putri-nayla <putri20820@gmail.com>
2024-08-30 11:11:39 +0700

    membuar percabangan bersarang

---
+++ 45 percabangan bersarang.php
@@ -0,0 +1,15 @@
+<?php
+
+$umur = 17;
+$menikah = false;
+
+if($umur > 18){
+    if($menikah){
+        echo "Selamat datang pak!";
+    } else {
+        echo "Selamat datang wahai pemuda!";
+    }
+} else {
+    echo "Maaf toko hanya terbuka untuk yang sudah 16+";
+}
+?>
