2d0eb4ade4166bc11a9ca55476e0900d98fff5f2
putri-nayla <putri20820@gmail.com>
2024-08-30 15:43:40 +0700

    membuat elseif

---
+++ 58 penggunaan elseif .php
@@ -0,0 +1,13 @@
+<?php
+$usia = 20;
+
+if ($usia < 13) {
+    echo "Kamu masih anak-anak.";
+} elseif ($usia >= 13 && $usia <= 19) {
+    echo "Kamu adalah remaja.";
+    print_r("<p><strong> by: putri nayla</strong>");
+} elseif ($usia > 19 && $usia <= 30) {
+    echo "Kamu adalah dewasa muda.";
+} else {
+    echo "Kamu adalah dewasa.";
+}
