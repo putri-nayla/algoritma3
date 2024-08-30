43edafcab67e79c8a6ee92bd605b032d459b7354
putri-nayla <putri20820@gmail.com>
2024-08-30 11:23:45 +0700

    membuat pernyataan switch

---
+++ 49 pernyataan switch.php
@@ -0,0 +1,16 @@
+<?php
+$grade = 'B';
+switch ($grade) {
+  case 'A':
+    echo "Luar biasa!";
+    break;
+  case 'B':
+  case 'C':
+    echo "Bagus!";
+    break;
+  default:
+    echo "Tetap semangat!";
+    print_r("<p><strong>by :putri nayla</strong>");
+    
+}
+
