3b30694a1a28762afa7984603117e8f1e534f324
putri-nayla <putri20820@gmail.com>
2024-08-30 10:20:31 +0700

    membuat strcmp dan strcasecmp

---
+++ 27 membuat operator. strcmp dan strcasecmp.php
@@ -0,0 +1,5 @@
+<?php
+echo strcmp("Hello World!", "Hello World!"); // Outputs: 0
+echo strcasecmp("hello world!", "HELLO WORLD!"); // Outputs: 0
+print_r("<p><strong>by :putri nayla</strong>");
+?>
