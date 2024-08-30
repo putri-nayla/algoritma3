c07f121d5af93ad14228e5cd5a1d2bce274e7557
putri-nayla <putri20820@gmail.com>
2024-08-30 09:27:03 +0700

    membuat operator bitwise

---
+++ 16 operator .bitwise .php
@@ -0,0 +1,36 @@
+<?php
+
+$a = 60;
+$b = 13;
+
+// bitwise AND
+$c = $a & $b;
+echo "$a & $b = $c";
+echo "<br>";
+
+$a= 7;
+$b = 34;
+
+// bitwise OR
+$c = $a | $b;
+echo "$a | $b = $c";
+echo "<br>";
+
+// bitwise XOR
+$c = $a ^ $b;
+echo "$a ^ $b = $c";
+echo "<br>";
+
+$a= 45;
+$b= 32;
+
+// Shift Left
+$c = $a << $b;
+echo "$a << $b = $c";
+echo "<br>";
+
+// Shift Right
+$c = $a >> $b;
+echo "$a >> $b = $c";
+echo "<br>";
+?>
