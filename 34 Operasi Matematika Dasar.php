3069331a1b8baa489b4991bb3afe5a36a015807e
putri-nayla <putri20820@gmail.com>
2024-08-30 10:42:17 +0700

    membuat matematika dasar

---
+++ 34 Operasi Matematika Dasar.php
@@ -0,0 +1,19 @@
+<?php
+$angka1 = 10;
+$angka2 = 5;
+
+// Penambahan
+echo $angka1 + $angka2; // Hasil: 15
+
+$a =10;
+$b=5;
+// Pengurangan
+echo $angka1 - $angka2; // Hasil: 5
+
+// Perkalian
+echo $angka1 * $angka2; // Hasil: 50
+
+// Pembagian
+echo $angka1 / $angka2; // Hasil: 2
+print_r("<p><strong>by: putri nayla</strong>");
+?>
