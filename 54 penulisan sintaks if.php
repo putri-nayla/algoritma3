05da907a1af44cd41fc3d977c0b6f0e34f36dc8a
putri-nayla <putri20820@gmail.com>
2024-08-30 15:24:57 +0700

    membuat penulisan sintaks

---
+++ 54 penulisan sintaks if.php
@@ -0,0 +1,10 @@
+<?Php
+if (kondisi) {
+    // kode yang akan dijalankan jika kondisi bernilai true
+}
+Penulisan Sintaks Else
+if (kondisi) {
+    // kode yang akan dijalankan jika kondisi bernilai true
+} else {
+    // kode yang akan dijalankan jika kondisi bernilai false
+}
