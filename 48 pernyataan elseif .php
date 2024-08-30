652214799ceb1986099eee13f0b5277e464d7f7d
putri-nayla <putri20820@gmail.com>
2024-08-30 11:21:12 +0700

    membuat pernyataan elseif

---
+++ 48 pernyataan elseif .php
@@ -0,0 +1,12 @@
+<?php
+$nilai = 80;
+if ($nilai >= 90) {
+  echo "Nilai kamu A";
+} elseif ($nilai >= 80) {
+  echo "Nilai kamu B";
+} else {
+  echo "Nilai kamu C";
+  print_r("<p><strong>by:putri nayla</strong>");
+  
+}
+
