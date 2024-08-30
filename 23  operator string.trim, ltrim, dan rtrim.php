fc052dfe335c0df4512b76e38c0752ad219d7799
putri-nayla <putri20820@gmail.com>
2024-08-30 10:03:26 +0700

    membuat trim,itrim,rtrim

---
+++ 23  operator string.trim, ltrim, dan rtrim.php
@@ -0,0 +1,7 @@
+<?php
+$text = " Hello World! ";
+echo "[" . trim($text) . "]"; // Outputs: "[Hello World!]"
+echo "[" . ltrim($text) . "]"; // Outputs: "[Hello World! ]"
+echo "[" . rtrim($text) . "]"; // Outputs: "[ Hello World!]"
+print_r("<p><strong> by: putri nayla </strong>");
+?>
